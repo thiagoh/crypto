e71fbec8f207c8f12a522b55a995f9948dd471dd
Thiago Andrade <thiagoh@gmail.com>
2015-07-11 20:51:11 -0300

    Revert "tests"
    This reverts commit 54b9ba4d45765bd8380105973ba63c3db39f30ff.

--- buildd.sh
+++ buildd.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-if [ ! -f cmocka-1.0.1.tar.xz ]; then
-	wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-	rm -rf cmocka-1.0.1
-	tar xf cmocka-1.0.1.tar.xz
-fi
+current_dir=$(pwd)
 
-rm -rf cmocka-build || true
-mkdir cmocka-build
+rm -rf $current_dir/build || true
+mkdir $current_dir/build
+cd $current_dir/build
 
-rm -rf build || true
-mkdir build
-
-cd cmocka-build
-cmake ../cmocka-1.0.1
-make
-
-cd ../build
+#cmake --debug-output .. 
 
 cmake -G"Eclipse CDT4 - Unix Makefiles" -D_ECLIPSE_VERSION=4.5 -D CMAKE_BUILD_TYPE=Debug ..
-#cmake --debug-output .. 
 
 mv .project ..
 mv .cproject ..
